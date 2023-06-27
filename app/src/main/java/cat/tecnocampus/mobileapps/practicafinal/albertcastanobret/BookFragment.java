158de5ca38303795bf8ddc882bdf5247147826f4
AlbertCastanoBret <acastanoi@edu.tecnocampus.cat>
2023-06-27 18:06:50 +0200

    commit 8

--- app/src/main/java/cat/tecnocampus/mobileapps/practicafinal/albertcastanobret/BookFragment.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practicafinal/albertcastanobret/BookFragment.java
@@ -6,17 +6,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class BookFragment extends Fragment {
 
-    private String mParam1;
-    private String mParam2;
+    private ImageView imageView;
+    private TextView title, subtitle, authorTitle;
 
     public BookFragment() {
     }
@@ -46,7 +51,25 @@ public class BookFragment extends Fragment {
         Bundle bundle = this.getArguments();
         if (bundle != null) {
             Book book = (Book) bundle.getSerializable("book");
-            Log.d("TAG", book.getVolumeInfo().getTitle());
+            SetupBook(view, book);
         }
+    }
+
+    private void SetupBook(View view, Book book){
+        imageView = view.findViewById(R.id.bookImageFragment);
+        title = view.findViewById(R.id.bookTitleFragment);
+        subtitle = view.findViewById(R.id.bookSubtitleFragment);
+        authorTitle = view.findViewById(R.id.bookAuthorFragment);
+
+        Glide.with(getContext())
+                .load(book.getVolumeInfo().getImageLinks() != null ? book.getVolumeInfo().getImageLinks().getThumbnail() : R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+                .into(imageView);
+
+        title.setText(book.getVolumeInfo().getTitle());
+        String subtitleText = book.getVolumeInfo().getSubtitle();
+        subtitle.setVisibility(TextUtils.isEmpty(subtitleText) ? View.GONE : View.VISIBLE);
+        subtitle.setText(subtitleText);
+        authorTitle.setText(book.getVolumeInfo().getAuthors() != null ? "By: " + TextUtils.join(", ", book.getVolumeInfo().getAuthors()) : "By: Unknown");
     }
 }
